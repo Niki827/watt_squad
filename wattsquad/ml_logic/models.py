1e1a91c0b599e7d6ae27d8e5dc881ffb6b8641f5
Anna-Maria Roydeva <aroydeva@me.com>
2024-11-29 14:41:45 +0100

    add fast api get method

--- wattsquad/ml_logic/models.py
+++ wattsquad/ml_logic/models.py
@@ -7,8 +7,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-
-# Importing pre processed data
+from wattsquad.ml_logic import preproc
 
 
 # creating an XGBRegressor model for solar production
@@ -16,13 +15,23 @@ def XGBRegressor_solar():
     '''
     This function will build an XGBRegressor to predict the solar production of the Norwegian Rye microgrid during the testing period.
     '''
+    # importing relevant data
+    train_data = pd.read_csv('raw_data/train.csv')
+    test_data = pd.read_csv('raw_data/test.csv')
 
-    # creating y_train dataframe
-    y_train = y.copy()['pv_production']
+    # creating y_train and y_test
+    y_train = train_data['pv_production'].copy()
+    y_test = test_data['pv_production'].copy()
 
-    # Importing y_test
-    y_test = pd.read_csv("raw_data/test.csv")
-    y_test = y_test['pv_production']
+    # creating X_train and X_test
+    X_train = train_data
+    X_train = X_train.drop(columns=['pv_production', 'wind_production', 'consumption', 'spot_market_price'])
+    X_test = test_data
+    X_test = X_test.drop(columns=['pv_production', 'wind_production', 'consumption', 'spot_market_price'])
+
+    # Preprocessing features
+    X_train_transformed = preproc.transform_data(X_train)
+    X_test_transformed = preproc.transform_data(X_test)
 
     #Creating X_val and y_val
     X_train_transformed, X_val, y_train, y_val = train_test_split(
@@ -48,18 +57,22 @@ def XGBRegressor_solar():
         X_train_transformed,
         y_train,
         eval_set=[(X_train_transformed, y_train), (X_val, y_val)],
+        verbose=False,
         early_stopping_rounds=5     # Retain early stopping
     )
+
+    print("➡️  model fitting done")
 
     # Make predictions
     y_pred = xgb_reg.predict(X_test_transformed)
 
-    # format predictions in a suitable dataframe
+    print("➡️  performed predictions")
+
+    #format predictions in a suitable dataframe
     predictions_df = pd.read_csv("raw_data/test.csv")
     predictions_df = predictions_df[['time']]
     predictions_df['pv_forecast'] = y_pred
 
     # renaming the column 'time' to 'timestamp' in order to integrate it into calculations.load_data()
     predictions_df.rename(columns={'time': 'timestamp'}, inplace=True)
-
     return predictions_df
